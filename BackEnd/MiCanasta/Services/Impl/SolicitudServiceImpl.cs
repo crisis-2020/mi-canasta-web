c3ec762045806255435d7db08264d55a3b7f8de7
JYellow363 <jyellow363@gmail.com>
2020-05-02 01:13:55 -0500

    Test (aún fallan)

--- BackEnd/MiCanasta/Services/Impl/SolicitudServiceImpl.cs
+++ BackEnd/MiCanasta/Services/Impl/SolicitudServiceImpl.cs
@@ -15,12 +15,24 @@ namespace MiCanasta.MiCanasta.Services.Impl
 
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly List<Familia> _familiaList;
 
         public SolicitudServiceImpl(ApplicationDbContext context,
             IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
+        }
+
+        public SolicitudServiceImpl()
+        {
+            _familiaList = new List<Familia>()
+            {
+                new Familia() { FamiliaId=1, AceptaSolicitudes=true, Nombre="LosGeniales", Cantidad=4 },
+                new Familia() { FamiliaId=2, AceptaSolicitudes=false, Nombre="Los Pollitos", Cantidad=5 },
+                new Familia() { FamiliaId=3, AceptaSolicitudes=true, Nombre="Omega", Cantidad=2 }
+
+            };
         }
 
         public List<SolicitudDto> GetAll()
---
+++ MiCanasta Test/SolicitudControllerTest.cs
@@ -0,0 +1,96 @@
+﻿using GenFu;
+using MiCanasta.MiCanasta.Controllers;
+using MiCanasta.MiCanasta.Dto;
+using MiCanasta.MiCanasta.Model;
+using MiCanasta.MiCanasta.Services;
+using MiCanasta.MiCanasta.Services.Impl;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MiCanasta_Test
+{
+	public class SolicitudControllerTest
+    {
+
+		SolicitudController _controller;
+		SolicitudService _service;
+
+		public SolicitudControllerTest()
+		{
+			_service = new SolicitudServiceImpl();
+			_controller = new SolicitudController(_service);
+		}
+
+
+		//Si la familia no existe
+		[Fact]
+		public void Add_InvalidObjectPassed_ReturnNotFound()
+		{
+			/* _service = new Mock<SolicitudService>();
+			var _controller = new SolicitudController(_service.Object);*/
+
+			// Arrange
+			SolicitudCreateDto model = new SolicitudCreateDto
+			{
+				Dni = "76697297",
+				FamiliaNombre = "Los pinguinos
+			};
+
+			// Act
+			ActionResult result = _controller.Create(model);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+
+		//Si la familia no acepta solicitudes
+		[Fact]
+		public void Add_ValidObjectPassed_ReturnBadRequest()
+		{
+			/* _service = new Mock<SolicitudService>();
+			var _controller = new SolicitudController(_service.Object);*/
+
+			// Arrange
+			SolicitudCreateDto model = new SolicitudCreateDto
+			{
+				Dni = "76697298",
+				FamiliaNombre = "Los Pollitos"
+			};
+
+
+			// Act
+			ActionResult result = _controller.Create(model);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+
+		//Flujo normal
+		[Fact]
+		public void Add_ValidObjectPassed_ReturnOk()
+		{
+			/* _service = new Mock<SolicitudService>();
+			var _controller = new SolicitudController(_service.Object);*/
+
+			// Arrange
+			SolicitudCreateDto model = new SolicitudCreateDto
+			{
+				Dni = "15665105",
+				FamiliaNombre = "LosGeniales"
+			};
+
+			// Act
+			ActionResult result = _controller.Create(model);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+	}
+}
