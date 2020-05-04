204205ced8efde6ca498ec1edd77ca74779361e7
Anthony Joaquin Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-05-04 18:24:31 -0500

    Se trabaja con el frontend de login

--- Frontend/src/app/core/services/auth.service.js
+++ Frontend/src/app/core/services/auth.service.js
@@ -1,28 +1,25 @@
 import axios from "axios";
 import { environments } from "../../environments/environments";
 export default class AuthService {
+  /**
+   * EJEMPLO PARA GET
+   */
 
+  // static async autenticacion(dni) {
+  //     return await axios({
+  //         url: `${environments.api}/${dni}`,
+  //         method: "GET"
+  //     })
+  // }
 
-    static async autenticacion(dni) {
-        return await axios({
-            url: `https://reniec-api.herokuapp.com/ciudadanos/${dni}`,
-            method: "GET"
-        })
-    }
-
-    /**
-     * EJEMPLOS PARA PETICIONES POST
-     */
-    
-     static async PeticionPost(){
-         return await axios({
-             url: `${environments.api}/registro`,
-             method: "POST",
-             data:{
-                "name": "Jair",
-                "edad": 20
-             }
-         })
-     }
-
+  static async autenticacion(usuario) {
+    return await axios({
+      url: `${environments.api}/usuarios`,
+      method: "POST",
+      data: {
+        dni: usuario.dni,
+        contrasena: usuario.contrasena,
+      },
+    });
+  }
 }
