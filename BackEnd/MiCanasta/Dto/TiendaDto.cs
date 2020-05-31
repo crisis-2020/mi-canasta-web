4679b234f624e31a3d39b0e57b0aebff4576ca7b
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-05-31 12:46:48 -0500

    Corrección errores

--- BackEnd/MiCanasta/Dto/TiendaDto.cs
+++ BackEnd/MiCanasta/Dto/TiendaDto.cs
@@ -37,19 +37,7 @@ namespace MiCanasta.MiCanasta.Dto
         public string Latitud { get; set; }
         public string Longitud { get; set; }
         public string Horario { get; set; }
-        public string Contrasena { get; set; }
-
-        public class TiendaUpdateDto
-        {
-            public string Descripcion { get; set; }
-            public string Direccion { get; set; }
-            public string Latitud { get; set; }
-            public string Longitud { get; set; }
-            public string Horario { get; set; }
-            public List<UsuarioTiendaDto> UsuarioTiendas { get; set; }
-            public List<StockDto> Stocks { get; set; }
-        }
-        
+        public string Contrasena { get; set; }  
         
     }
     public class TiendaUsuarioDto
