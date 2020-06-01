9bfc0c2cb0ddf811ae14039ff9d20b0161943c69
JYellow363 <jyellow363@gmail.com>
2020-06-01 17:38:47 -0500

    HU21 Terminada
    Desarrollo de Backend: http-method POST->  "/historiales" -> Se agregar el campo confirmacion a la tabla historiales

---
+++ BackEnd/MiCanasta/Controllers/HistorialController.cs
@@ -0,0 +1,33 @@
+﻿using MiCanasta.MiCanasta.Dto;
+using MiCanasta.MiCanasta.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiCanasta.MiCanasta.Controllers
+{
+    [ApiController]
+    [Route("historiales")]
+    public class HistorialController: ControllerBase
+    {
+        private readonly HistorialService _historialService;
+        public HistorialController(HistorialService HistorialService)
+        {
+            _historialService = HistorialService;
+        }
+
+        [HttpPost]
+        public ActionResult Create(HistorialCreateDto model)
+        {
+            try
+            {
+                return Created("", _historialService.Create(model));
+            }
+            catch (Exception) {
+                return BadRequest();
+            }
+        }
+    }
+}
