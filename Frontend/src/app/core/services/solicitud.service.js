334db68047884a97c46ddcaa456cf018065a9f05
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-06-15 18:27:20 -0500

    anadir funcionalidad al frontend - INCOMPLETO
    INCOMPLETO

--- Frontend/src/app/core/services/solicitud.service.js
+++ Frontend/src/app/core/services/solicitud.service.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { environments } from "../../environments/environments";
 export default class SolicitudService {
-    static async crearSolicitud(solicitudRequest) {
-        return await axios
-            .post(`${environments.api}/solicitudes`, solicitudRequest)
-    }
-    static async obtenerSolicitudes(dni) {
-        return await axios
-            .get(`${environments.api}/solicitudes/${dni}`)
-    }
+  static async crearSolicitud(solicitudRequest) {
+    return await axios.post(
+      `${environments.api}/solicitudes`,
+      solicitudRequest
+    );
+  }
+  static async obtenerSolicitudes(dni) {
+    return await axios.get(`${environments.api}/solicitudes/${dni}`);
+  }
+  static async denegarSolicitud(dni) {
+    return await axios.delete(`${environments.api}/solicitudes/${dni}`);
+  }
 }
--- Frontend/src/app/modules/requests-received/requests-received.page.vue
+++ Frontend/src/app/modules/requests-received/requests-received.page.vue
@@ -22,28 +22,27 @@
 import RequestCard from "./components/request-received.card.vue";
 export default {
   name: "RequestReceived",
-  components: {RequestCard},
-  data:function(){
-      return{
-          mock: [
-              {title:'Solicitud 10', nombre: 'Felipe Gomez', dni:'73044851'},
-              {title:'Solicitud 10', nombre: 'Felipe Gomez', dni:'73044851'},
-              {title:'Solicitud 10', nombre: 'Felipe Gomez', dni:'73044851'},
-              {title:'Solicitud 10', nombre: 'Felipe Gomez', dni:'73044851'}
-          ]
-      }
-  }
+  components: { RequestCard },
+  data: function() {
+    return {
+      mock: [
+        { title: "Solicitud 10", nombre: "Felipe Gomez", dni: "12345674" },
+        { title: "Solicitud 10", nombre: "Felipe Gomez", dni: "73044851" },
+        { title: "Solicitud 10", nombre: "Felipe Gomez", dni: "73044851" },
+        { title: "Solicitud 10", nombre: "Felipe Gomez", dni: "73044851" },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
-.request-received-container{
-    width: 100%;
-    padding: 24px 32px;
-    height: calc(100vh - 128px);
-    overflow-y: auto;
+.request-received-container {
+  width: 100%;
+  padding: 24px 32px;
+  height: calc(100vh - 128px);
+  overflow-y: auto;
 }
-
 
 .request-received__title {
   margin-top: 24px;
@@ -55,17 +54,16 @@ export default {
   color: #000000;
   text-align: left;
 }
-.request-received__allow{
-    margin-bottom: 24px;
+.request-received__allow {
+  margin-bottom: 24px;
 }
-.request-received__allow p{
-    margin-bottom: 8px;
+.request-received__allow p {
+  margin-bottom: 8px;
 }
-.request-received__list{
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+.request-received__list {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 }
-
 </style>
