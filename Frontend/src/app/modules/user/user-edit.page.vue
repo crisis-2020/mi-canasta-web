f51fb5f11a6a30ba3538ebe5347f9da4a27365a3
JYellow363 <jyellow363@gmail.com>
2020-06-20 00:54:25 -0500

    hu15 terminada
    Con las tres validaciones de los exceptions.

--- Frontend/src/app/modules/user/user-edit.page.vue
+++ Frontend/src/app/modules/user/user-edit.page.vue
@@ -1,52 +1,65 @@
 <template>
     <div class="user-edit-container">
-        <div class="user-edit-container__form-container">
-                <h1 class="user-edit-container__header h1">Editar información</h1><br>
-            <div class="input-shared-container">
-                <label>Correo</label>
-                <input
-                    class="input-shared-component"
-                    v-model="correo"
-                />
+        <div class="field-container margin">
+            <img class = "image-back" src="../../../assets/ic_back.svg" alt=""
+            v-on:click="volver">
+            <h1 class="user-edit-container__header h1">
+                Editar información
+            </h1>
+            <div class="user-edit-container__form-container">
+                <div class="input-shared-container">
+                    <label>Correo</label>
+                    <input
+                        class="input-shared-component"
+                        v-model="correo"
+                    />
+                </div>
+                <div class="input-shared-container">
+                    <label>Contraseña Actual</label>
+                    <input
+                        type="password"
+                        class="input-shared-component"
+                        v-model="contrasena"
+                    />
+                </div>
+                <div class="input-shared-container">
+                    <label>Nueva contraseña</label>
+                    <input
+                        class="input-shared-component"
+                        type="password"
+                        placeholder="Opcional"
+                        v-model="nuevaContrasena"
+                    />
+                </div>
+                <div class="input-shared-container">
+                    <label>Repetir contraseña</label>
+                    <input
+                        class="input-shared-component"
+                        type="password"
+                        placeholder="Opcional"
+                        v-model="repetirContrasena"
+                    />
+                </div>
+                <div class="user-edit-container__button">
+                    <ButtonShared
+                    class="user-edit-button"
+                    :text="'Actualizar'"
+                    :type="'large'"
+                    :bgColor="'red'"
+                    :loading="loadingButton"
+                    :disabled="contrasena.length < 1"
+                    @Event="putUsuario"
+                    />
+                </div>
             </div>
-            <div class="input-shared-container">
-                <label>Contraseña Actual</label>
-                <input
-                    class="input-shared-component"
-                    v-model="contrasena"
-                />
-            </div>
-            <div class="input-shared-container">
-                <label>Nueva contraseña</label>
-                <input
-                    class="input-shared-component"
-                    v-model="nuevaContrasena"
-                />
-            </div>
-            <div class="input-shared-container">
-                <label>Repetir contrasena</label>
-                <input
-                    class="input-shared-component"
-                    v-model="repetirContrasena"
-                />
-            </div>
-
-            <ButtonShared
-                class="login-container__button"
-                :text="'Actualizar'"
-                :type="'large'"
-                :bgColor="'red'"
-                :loading="loadingButton"
-                :disabled="loadingButton"
-            />
-
-            <ErrorModalShared
-                @Event="closeModal"
-                v-if="isShowModalError"
-                :title="error.title"
-                :description="error.description"
-            />
         </div>
+        <ErrorModalShared
+            @Event="closeModal"
+            v-if="isShowModal"
+            :title="modalTitle"
+            :description="modalDescription"
+        ></ErrorModalShared>
+
     </div>
 </template>
 
@@ -64,13 +77,14 @@ import ErrorModalShared from "../../shared/modal/error-modal.component.vue";
         data: function() {
             return {
                 correo:"",
-                contrasena:"******",
-                nuevaContrasena: "Opcional",
-                repetirContrasena: "Opcional",
+                contrasena:"",
+                nuevaContrasena: "",
+                repetirContrasena: "",
                 user: UsuarioGet,
                 loadingButton: false,
-                isShowModalError: false,
-
+                isShowModal: false,
+                modalDescription: "",
+                modalTitle: "",
             }
         },
 
@@ -84,62 +98,98 @@ import ErrorModalShared from "../../shared/modal/error-modal.component.vue";
                 try {
                     const res = await UsuarioService.getUsuario(localStorage.getItem("dni"));
                     this.user = res.data;
-                    this.correo = this.user.correo;
-                    console.log(this.user);  
+                    this.correo = this.user.correo;  
                 }
                 catch (error) {
                     console.log(error);        
                 }
             },
 
-            closeModal() {
-                this.isShowModalError = false;
+            async putUsuario(){
+                this.loadingButton = true;
+                try {
+                    await UsuarioService.putUsuario(localStorage.getItem("dni"), {
+                        correo: this.correo,
+                        contrasena: this.contrasena,
+                        nuevaContrasena: this.nuevaContrasena,
+                        repetirContrasena: this.repetirContrasena,
+                    });
+                    this.loadingButton = false;
+                    this.modalTitle="Éxito";
+                    this.modalDescription="Se realizó la actualización de datos";
+                    this.isShowModal = true;
+                }
+                catch (error) {
+                    this.loadingButton = false;
+                    if (error.response.data.exception === "ActualPasswordNotMatchException" ||
+                    error.response.data.exception === "NewPasswordNotMatchException" ||
+                    error.response.data.exception === "EmailWrongFormatException"){
+                        this.modalDescription = error.response.data.message;
+                        this.modalTitle = "Error"
+                        this.isShowModal = true;
+                    }
+                    console.log(error);
+                }
             },
 
-            actualizarDatos(){}
+            volver(){
+                this.$router.push("/home/user");
+            },
+
+            closeModal() {
+                this.isShowModal = false;
+                location.reload();
+            },
         }
     }
 </script>
 
 <style>
 
-.user-edit-container {
-    margin: 0px 40px 160px 40px;
-    position: relative;
-    width: 100%;
-    height: 100%;
-}
-
-.user-edit-containerr__header {
-    padding-top: 64px;
+.field-container {
+    margin: 0px 40px 0px 40px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    position: relative;
 }
 
 .user-edit-container h1 {
-
-    font-size: 20px;
-    font-weight: 700;
-    margin: 16px 0 8px 0;
-}
-.user-edit-container__header p {
-    font-size: 14px;
-}
-.user-edit-container__form-container {
-    display: flex;
     justify-content: right;
     align-items: right;
+    font-size: 20px;
+    font-weight: 700;
+    margin: 12px 0 8px 0;
+}
+
+.user-edit-container__form-container {
+    justify-content: center;
+    align-items: center;
+    display: flex;
     flex-direction: column;
-    margin-top: 8px;
+    margin-top: 6px;
 }
 
 .user-edit-container__button {
-  margin-top: 14px;
+    justify-content: center;
+    align-items: center;
+    margin: auto auto auto auto
 }
+
+.user-edit-button {
+    margin: 12px auto 0 auto;
+}
+
 .input-shared-container {
-  margin-bottom: 24px;
+  margin-bottom: 14px;
+}
+
+.image-back {
+    display: block;
+    float: left;
+    width: 25px;
+    height: 25px;
+    margin-right: 6px;
+    margin-top: 6px;
 }
 
 </style>
